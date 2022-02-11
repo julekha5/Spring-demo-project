3ae693d908d7f578777963d8e824dd7b8a97725e
Julekha5 <mulani.julekha93@gmail.com>
2022-02-11 11:01:40 +0530

    Added : Spring dependency injection example using constructor

--- src/main/java/com/bridgelabz/di/Exam.java
+++ src/main/java/com/bridgelabz/di/Exam.java
@@ -4,14 +4,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Exam {
-	
-	public static void main(String[] args) {
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-	    Student student1 = context.getBean("student1", Student.class);
-	    student1.displayInfo();
-	    
-	    Student student2 = context.getBean("student2", Student.class);
-	    student2.displayInfo();	}
 
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		// using setter injection
+//	    Student student1 = context.getBean("student1", Student.class);
+//	    student1.displayInfo();
+//	    
+//	    Student student2 = context.getBean("student2", Student.class);
+//	    student2.displayInfo();	
+
+		// using constructor injection
+		System.out.println("Two parameterized constructor called ");
+		Student student3 = context.getBean("student3", Student.class);
+		student3.displayInfo();
+		
+		System.out.println("single parameterized constructor called ");
+		Student student4 = context.getBean("student4", Student.class);
+		student4.displayInfo();
+	}
 }
--- src/main/java/com/bridgelabz/di/Student.java
+++ src/main/java/com/bridgelabz/di/Student.java
@@ -5,17 +5,26 @@ public class Student {
 	private int id;
 	private String studentName;
 
-    //Using setter method dependency injection
-	public void setId(int id) {
+	// Using constructor dependency injection
+	public Student(int id, String studentName) {
 		this.id = id;
-		System.out.println("setter method called :" + ":setId");
-	}
-
-	public void setStudentName(String studentName) {
 		this.studentName = studentName;
-		System.out.println("setter method called :" + ":setStudentName");
-
 	}
+
+	public Student(int id) {
+		this.id = id;
+	}
+	// Using setter method dependency injection
+//	public void setId(int id) {
+//		this.id = id;
+//		System.out.println("setter method called :" + ":setId");
+//	}
+//
+//	public void setStudentName(String studentName) {
+//		this.studentName = studentName;
+//		System.out.println("setter method called :" + ":setStudentName");
+//
+//	}
 
 	public void displayInfo() {
 		System.out.println("Student name is : " + studentName + " and id is : " + id);
